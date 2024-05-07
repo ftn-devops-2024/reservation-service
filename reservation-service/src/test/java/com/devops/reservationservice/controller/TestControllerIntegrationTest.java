e9c3505d986f73462bc35068be5af4ada60e00b6
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-07 15:25:33 +0200

    test: add integration test using test containers

---
+++ reservation-service/src/test/java/com/devops/reservationservice/controller/TestControllerIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.devops.reservationservice.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+    @Test
+    public void testGetTestEndpoint() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/test")
+                .then()
+                .statusCode(200);
+    }
+}
