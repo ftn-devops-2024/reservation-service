f9148a6260ee2acb542ce8c7fd02ef682c3f76ee
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-27 23:46:36 +0200

    fix: add test application properties

--- reservation-service/src/test/java/com/devops/reservationservice/controller/TestControllerIntegrationTest.java
+++ reservation-service/src/test/java/com/devops/reservationservice/controller/TestControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -17,6 +18,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class TestControllerIntegrationTest {
 
     @Autowired
