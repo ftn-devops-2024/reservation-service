555a1206bc819b7347aca77ff401a5115c949c8a
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-29 22:34:39 +0200

    test: test pipeline

--- reservation-service/src/test/java/com/devops/reservationservice/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/devops/reservationservice/ReservationServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package com.devops.reservationservice;
 
+import com.devops.reservationservice.controller.TestController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
+@WebMvcTest(TestController.class)
+@ActiveProfiles("test")
 class ReservationServiceApplicationTests {
 
 	@Test
