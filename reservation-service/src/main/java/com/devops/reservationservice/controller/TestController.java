fa866bd15bcc044ef2e7a593e9a8b2cc4039b54e
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-06-18 23:53:47 +0200

    feat: Add local configuration for loki, prometheus and tempo

--- reservation-service/src/main/java/com/devops/reservationservice/controller/TestController.java
+++ reservation-service/src/main/java/com/devops/reservationservice/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.devops.reservationservice.controller;
 
+import com.devops.reservationservice.service.ReservationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    Logger logger = LoggerFactory.getLogger(ReservationService.class);
+
     @GetMapping("/test")
     public String test(){
+        logger.info("reservation ideee");
         return "Welcome from reservation-service";
     }
 }
